f9629a425728e3a02469fafa9a0e9736a2fd3442
김상협 <dudwogkrrmq@gmail.com>
2024-06-17 17:46:51 +0900

    - 주석 제거, Ignore 추가, 건물&나무 에셋 추가

--- SimpleVRProject/Source/SimpleVRProject/Animation/Notify/AmmoEjectAnimNotify.cpp
+++ SimpleVRProject/Source/SimpleVRProject/Animation/Notify/AmmoEjectAnimNotify.cpp
@@ -49,37 +49,5 @@ void UAmmoEjectAnimNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenc
 	
 	Dispenser->SpawnEjectCartridge(FinalTransform);
 	Dispenser->SpawnProjectile(MuzzleFlashTransform);
-	/*
-
 	
-
-	{
-
-		AEjectCartridge* EjectCartridge = MeshComp->GetWorld()->SpawnActor<AEjectCartridge>(AEjectCartridge::StaticClass(),
-			FinalTransform);
-
-		const FVector RightVector = EjectCartridge->GetActorRightVector();
-		const double RandomVector = FMath::RandRange(50.0, 100.0);
-		const FVector Impulse = RightVector * RandomVector;
-
-		EjectCartridge->Init(ProjectileDT->EjectStaticMesh, Impulse);
-	}*/
-	//{
-	//	//const FTransform FinalTransform = ProjectileDT->BulletProceduralMeshTransform * MuzzleFlashTransform;
-
-	//	AProjectile* Bullet = MeshComp->GetWorld()->SpawnActorDeferred<AProjectile>
-	//		(AProjectile::StaticClass(), MuzzleFlashTransform);
-
-	//	const FVector ForwardVector = Bullet->GetActorForwardVector();
-	//	const float BulletSpeed = ProjectileDT->ProjectileSpeed;
-	//	const float RandSpeed = FMath::RandRange(BulletSpeed - 10, BulletSpeed + 10);
-	//	const FVector Impulse = ForwardVector * RandSpeed;
-
-	//	Bullet->Init(ProjectileDT->BulletStaticMesh, ProjectileDT->BulletProceduralMeshTransform, Impulse, ProjectileDT->GunShotEffect);
-	//	
-	//	const FTransform FinalTransform = ProjectileDT->BulletProceduralMeshTransform * MuzzleFlashTransform;
-	//	
-	//	Bullet->FinishSpawning(FinalTransform, true);
-	//	Bullet->GetProceduralMeshComponent()->AddImpulse(Impulse);
-	//}
 }
